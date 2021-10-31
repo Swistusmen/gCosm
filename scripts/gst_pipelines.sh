4e835743abaf72e4bd6af91a1ceb717be23fba2e
Swistusmen <michal76aw@gmail.com>
2021-10-31 17:07:34 +0100

    Pipelines prototypes and plan

--- scripts/gst_pipelines.sh
+++ scripts/gst_pipelines.sh
@@ -21,7 +21,7 @@ gst-launch-1.0 -v filesrc location=rabbit.mp4 ! x264enc ! rtph264pay name=pay0 p
 #transcode h.264-> av1 in mp4, aac->aac
 gst-launch-1.0 -e filesrc location=earth.mp4 ! \decodebin name=decode ! \videoscale ! 'video/x-raw,width=640,height=480' ! \av1enc ! queue ! mp4mux name=mp4mux ! filesink location=0000.mp4 \decode. ! audioconvert ! voaacenc bitrate=128 ! queue ! mp4mux.
 
-#stream mp4 with av1 and aac
+#stream mp4 with av1 and aac! \hlssink playlist-root=http://127.0.0.1:8080 target-duration=5 max-files=5
 
 
 #receive mp4 and save to file (av1+aac)
@@ -43,3 +43,43 @@ gst-launch-1.0 rtspsrc location=rtsp://127.0.0.1:8554/test name=src src. ! decod
 gst-launch-1.0 filesrc location=rabbit.mp4 ! decodebin ! mpegtsmux ! \hlssink playlist-root=http://127.0.0.1:8080 target-duration=5 max-files=5
 python3 -m http.server 8080 #in the location where there is a server
 #accessible on http//:127.0.0.1:8080/playlist.m3u8
+
+#another one:
+gst-launch-1.0 filesrc location=rabbit.mp4 ! qtdemux ! queue ! decodebin ! x264enc ! mpegtsmux ! \hlssink playlist-root=http://127.0.0.1:8080 target-duration=5 max-files=5
+
+#streamming av1 on ubuntu 21.1.0 (formating to av1)
+gst-launch-1.0 filesrc location=rabbit.mp4 ! qtdemux ! queue ! decodebin ! av1enc ! matroskamux ! \hlssink playlist-root=http://127.0.0.1:8080 target-duration=5 max-files=5
+
+#receiving av1 on ubuntu 21.1.0
+gst-launch-1.0 -v souphttpsrc location=http://127.0.0.1:8080/playlist.m3u8 ! filesink location=sample.mkv
+
+#stream from file
+gst-launch-1.0 filesrc location=rabbit.mp4 ! qtdemux ! queue ! decodebin ! x264enc ! mpegtsmux ! \hlssink playlist-root=http://127.0.0.1:8080 target-duration=5 max-files=100
+
+#stream matroska file http- no transcoding:
+gst-launch-1.0 filesrc location=fat_bunny.mkv -v ! matroskademux ! h264parse ! matroskamux ! \hlssink playlist-root=http://127.0.0.1:8080 target-duration=5 max-files=5
+
+#transcode from mp4 to mkv (h264->av1) #TODO: 
+gst-launch-1.0 -e filesrc location=rabbit.mp4 ! \decodebin name=decode ! av1enc ! queue ! matroskamux name=mux ! filesink location=rabbitAV1.mkv \decode. ! audioconvert ! voaacenc bitrate=128 ! queue ! mux.
+
+#stream AV1 mkv-> mkv (no transcoding)- only video: FIXME: TODO:
+gst-launch-1.0 -e filesrc location=rabbitAV1.mkv -v ! matroskademux ! matroskamux ! \hlssink playlist-root=http://127.0.0.1:8080 target-duration=5 max-files=100
+
+#save hls stream to file (mkv-> mkv) (av1->av1), only video: FIXME: TODO: 
+gst-launch-1.0 -v souphttpsrc location=http://127.0.0.1:8080/playlist.m3u8 -e ! hlsdemux ! matroskademux ! matroskamux ! filesink location=sample2.mkv
+
+#hls stream mp4 h264, aac TODO:
+gst-launch-1.0 filesrc location=rabbit.mp4 ! qtdemux name=demux ! queue ! h264parse disable-passthrough=true ! mpegtsmux name=mux ! hlssink playlist-root=http://127.0.0.1:8080 max-files=100 playlist-length=0 demux. ! queue ! aacparse ! mux.
+
+#hls receiver mp4 h264 aac TODO:
+gst-launch-1.0 -v souphttpsrc location=http://127.0.0.1:8080/playlist.m3u8 -e ! hlsdemux ! tsdemux name=demux ! queue ! h264parse ! mp4mux name=mux ! filesink location=sample2.mp4 demux. ! queue ! aacparse ! mux.
+
+#hls stream mkv av1, aac TODO:
+gst-launch-1.0 -e filesrc location=rabbitAV1.mkv -v ! matroskademux name=demux ! queue ! matroskamux name=mux ! \hlssink playlist-root=http://127.0.0.1:8080 target-duration=5 max-files=100 demux. ! queue ! aacparse ! mux.
+
+#hls receive mkv av1, aac TODO:
+gst-launch-1.0 -v souphttpsrc location=http://127.0.0.1:8080/playlist.m3u8 -e ! hlsdemux ! matroskademux name=demux ! queue  ! matroskamux name=mux ! filesink location=sample3.mkv demux. ! queue ! aacparse ! mux.
+
+#hls stream mp4 (h26, aac)->mkv (av1,aac) TODO: -but in the future
+gst-launch-1.0 filesrc location=rabbit.mp4 ! qtdemux name=demux ! queue ! h264parse ! avdec_h264 ! av1enc ! matroskamux name=mux ! hlssink playlist-root=http://127.0.0.1:8080 max-files=100 playlist-length=0 demux. ! queue ! aacparse ! mux.
+
